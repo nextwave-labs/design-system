0c2d90338efa16c860667ea321c6f5b44b649384
Jose Velasquez <velasquezdenorajose@gmail.com>
2026-09-14 23:31:31 -0400

    feat(release): configure scoped package publishing

--- release.config.js
+++ release.config.js
@@ -15,14 +15,14 @@ export default {
     [
       '@semantic-release/npm',
       {
-        prepareCmd: 'npm run build',
+        prepareCmd: 'pnpm build',
       },
     ],
     '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        assets: ['CHANGELOG.md', 'package.json', 'pnpm-lock.yaml'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
