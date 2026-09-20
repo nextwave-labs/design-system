df5eb12761bc1d6be2bebdc6822602f4aa965056
Bryan Stanlyn García Nuñez <bryan1997st@gmail.com>
2026-09-20 00:37:15 -0500

    feat(storybook): add comprehensive configuration documentation and assets
    - Moves Configure.mdx file for detailed project configuration guidance in .storybook instead of stories
    - Moves files from stories to .storybook and components stories inside the component folder

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from './Button'
+
+const meta: Meta<typeof Button> = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: 'radio',
+      options: ['primary', 'secondary'],
+    },
+    size: {
+      control: 'radio',
+      defaultValue: 'md',
+      defaultRadio: 'md',
+      options: ['sm', 'md', 'lg'],
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Button',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+    children: 'Button',
+  },
+}
